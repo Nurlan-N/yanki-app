327c0241417654c7f9e4acae5e32d77e67f8b35f
Nurlan-N <Nurlanbn@code.edu.az>
2023-04-10 11:27:51 +0400

    Added Shop Item

---
+++ src/components/ShopItemBlock/index.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import imageUrl from '../../assets/img/categoryimg/1.png';
+import styles from './ShopItem.module.scss';
+import wishlistIcon from '../../assets/img/icon/productWishlist.png'
+
+console.log(styles);
+const index = () => {
+  return (
+    <div className={styles.wrapper}>
+      <div className={styles.shop_block}>
+        <a href="/shop/8">
+          <img className="image" src={imageUrl} alt="Pizza" />
+          <h4 className={styles.title}>Синее пальто</h4>
+          <h5 className={styles.price}>2900$</h5>
+          <a className={styles.wishlist}><img src={wishlistIcon} alt="" /></a>
+        </a>
+        <div className="shop-block__bottom">
+          <div className={styles.size}>
+            <p>xxs</p>
+            <p>xs</p>
+            <p>xl</p>
+          </div>
+          <div className={styles.color}>
+              <button className={styles.c1}></button>
+              <button className={styles.c2}></button>
+              <button className={styles.c3}></button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default index;
--- src/pages/Shop.jsx
+++ src/pages/Shop.jsx
@@ -1,10 +1,47 @@
 import React from 'react';
 import righRow from '../assets/img/icon/right.png';
 import { Link } from 'react-router-dom';
+import Select from 'react-select'
+import ShopItemBlock from '../components/ShopItemBlock'
+
+const sizeOptions = [
+  { value: 'xl', label: 'XL' },
+  { value: 'xxl', label: 'XXL' },
+  { value: 'ms', label: 'MS' }
+]
+const SizeSelect = () => (
+  <Select className='size-select' options={sizeOptions} placeholder="Size.."/>
+)
+const colorOptions = [
+  { value: 'xl', label: 'XL' },
+  { value: 'xxl', label: 'XXL' },
+  { value: 'ms', label: 'MS' }
+]
+const ColorSelect = () => (
+  <Select className='color-select' options={colorOptions} placeholder="Color.."/>
+)
+const priceOptions = [
+  { value: 'xl', label: 'XL' },
+  { value: 'xxl', label: 'XXL' },
+  { value: 'ms', label: 'MS' }
+]
+const PriceSelect = () => (
+  <Select className='price-select' options={priceOptions} placeholder="Price.."/>
+)
+const sortOptions = [
+  { value: '0', label: 'Relevance' },
+  { value: '1', label: 'Name (A - Z)' },
+  { value: '2', label: 'Name (Z - A)' },
+  { value: '3', label: 'Price (Low &gt; High)' },
+  { value: '4', label: 'Rating (Lowest)' }
+]
+const SortSelect = () => (
+  <Select className='color-select' options={sortOptions} placeholder="Sort By.."/>
+)
 
 const Shop = () => {
   return (
-    <div className="root">
+    <div className="show-wrapper">
       <div className="container">
         <div className="map d-flex ">
           <p>Home</p>
@@ -12,23 +49,41 @@ const Shop = () => {
           <p>Shop</p>
         </div>
         <div className="row">
-          <div className="content">
-          <div className="category-list">
-            <h4>Category</h4>
-            <Link to="/">Parks</Link>
-            <Link to="/">Fur coats</Link>
-            <Link to="/">Coat</Link>
-            <Link to="/">Jackets</Link>
-            <Link to="/">Fur coats</Link>
-            <Link to="/">Parks</Link>
-            <Link to="/">Coat</Link>
-          </div>
-          <div className="filter">
-            <div className="size">
-              <h5>SIZE</h5>
+          <div className="content d-flex">
+            <div className="category-list">
+              <h4>Category</h4>
+              <Link to="/">Parks</Link>
+              <Link to="/">Fur coats</Link>
+              <Link to="/">Coat</Link>
+              <Link to="/">Jackets</Link>
+              <Link to="/">Fur coats</Link>
+              <Link to="/">Parks</Link>
+              <Link to="/">Coat</Link>
             </div>
-          </div>
-          <div className="items">test</div>
+            <div className="gallary">
+              <div className="filter ">
+                <div className="size d-flex">
+                  {SizeSelect()}
+                  {ColorSelect()}
+                  {PriceSelect()}
+                  {SortSelect()}
+                </div>
+              </div>
+              <div className="shop-block">
+                <div className="items d-flex justify-content-between">
+                  <ShopItemBlock/>
+                  <ShopItemBlock/>
+                  <ShopItemBlock/>
+                  <ShopItemBlock/>
+                  <ShopItemBlock/>
+                  <ShopItemBlock/>
+                  <ShopItemBlock/>
+                  <ShopItemBlock/>
+                  <ShopItemBlock/>
+
+                </div>
+              </div>
+            </div>
           </div>
         </div>
       </div>
